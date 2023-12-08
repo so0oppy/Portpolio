a3cff79f4392e7fcd70d4463ee7b33ae5c8df745
Subin <80036437+hansb1023@users.noreply.github.com>
2023-12-08 13:53:06 +0900

    Feat: Intro page

--- portpolio-react/src/components/Intro.jsx
+++ portpolio-react/src/components/Intro.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import photo from "../assets/img/selfie.png";
+
 const Intro = () => {
-    return <div>Intro</div>;
+    return(
+        <section id="intro">
+            <div class="intro_inner">
+                <div class="image">
+                    <img src={photo} alt="프로필"/>
+                </div>
+                <div class="text">
+                    <div>Let's live a life 
+                        <br></br>of growth and dreaming.
+                    </div>
+                    <div>인터랙티브 미디어를 활용해 특별한 경험을 제작하는
+                        <br></br>가상현실 엔지니어를 꿈꾸고 있습니다.
+                    </div>
+                    <div>현재 경기도에 거주하고 있습니다.</div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Intro;
